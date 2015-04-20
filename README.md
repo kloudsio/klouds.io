5ca6b958915f1bcbc9a8fe3083b97ee280329cef
Daniel Sont <dan.sont@gmail.com>
2015-04-20 13:43:09 +0700

    how about some gh pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# MicroGrid
-
-The Open-Governance Micro Container Cloud.
-
-Get Involved at http://microgrid.github.io
--- stripe/index.js
+++
@@ -1,8 +0,0 @@
-
-
-var koa = require('koa')();
-
-koa.use(function* (next) {
-	var stripeToken = request.body.stripeToken;
-	this.body =
-});
