bf2d89d0364fe076933d2810f5f09794ad1c7532
Daniel Sont <dan.sont@gmail.com>
2015-05-04 13:20:28 +0700

    embarassing codestyle tweet

--- front-end/client/lib/api.js
+++ front-end/client/lib/api.js
@@ -4,19 +4,19 @@ var transforms = require('/lib/transforms.js');
 var state = require('/lib/state.js');
 
 var apiSpec = {
-	user: {
-		register: null,
-		login: login,
-	},
+  user: {
+    register: null,
+    login: login,
+  },
 
-	subscription: {
-		create: null,
-		delete: null,
-	}
+  subscription: {
+    create: null,
+    delete: null,
+  }
 };
 
 function* login(data) {
-	var res = yield qwest.post('/user/login', data, state.qwest);
+  var res = yield qwest.post('/user/login', data, state.qwest);
   transforms.login(res);
   return res;
 }
