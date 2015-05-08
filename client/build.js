5eec12d589d5305581a79937f9d4266f482bb5cd
Tuan Pham Anh <baabeetaa@gmail.com>
2015-05-08 16:32:14 +0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	lib/main.js
    
    use Idea ide

--- client/build.js
+++ client/build.js
@@ -1,23 +1,54 @@
-#!/bin/iojs
+var write = require('fs').writeFileSync;
+var basename = require('path').basename;
+var join = require('path').join;
+var Duo = require('duo');
 
-var duo = require('duo')(__dirname + '/src');
-var co = require('co');
 
-var plugins = require('./duo-plugins');
+/**
+ * Plugins
+ */
 
-duo
-	.installTo('./duo_modules')
-	.use(plugins)
-	.cache(false)
+var handlebars = require('duo-handlebars')
+var babel = require('duo-babel')
+var Watch = require('duo-watch')
 
-co(function* () {
-	var results = {
-		js: yield duo.entry('main.js').write(),
-		css: yield duo.entry('main.css').write(),
-	};
+var plugins = [];
 
-	console.dir(results);
+plugins.push(handlebars());
+plugins.push(babel({
+  ignore: /duo_modules|node_modules/,
+  onlyLocals: true,
+}));
 
-}).catch(function (e) {
-	console.error(e.stack);
+/**
+ * Dir
+ */
+var root = __dirname;
+var build = join(root, 'build');
+var src = join(root, 'src');
+
+/**
+ * Watch
+ */
+
+Watch(src).watch(function (file) {
+  console.log('changed: %s', file);
+  var out = join(build, basename(file));
+
+  var duo = Duo(src)
+    .installTo('duo_modules')
+    .token(process.env.TOKEN)
+    .use(plugins)
+    .cache(false)
+    .entry('app.css')
+    .entry('app.js')
+
+
+  duo.run(function (err, str) {
+    err && console.error(err);
+    write(out, src);
+    console.log('rebuilt: %s', file);
+  });
 });
+
+console.log('waiting for changes...');
